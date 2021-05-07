b851849ddd2a05741f419acdc66a5fda5670c123
loohuiming <43126456+loohuiming@users.noreply.github.com>
2021-05-07 11:22:25 +0800

    AddTransaction router

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Login } from './components/Login';
-import { AddTransaction } from './components/AddTransaction';
+import AddTransaction from './components/AddTransaction';
 
 export default function Router() {
     const LoginContainer = () => (
