4de65a836f076771786caaa6b82d13c580b2b68b
loohuiming <43126456+loohuiming@users.noreply.github.com>
2021-05-07 12:58:32 +0800

    name to input

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,33 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function AddTransaction() {
-    useEffect(() => {
-        async function postTransaction() {
-            let transactionAPI = 'https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add'
-            axios.defaults.headers.common = {
-                "X-API-Key": "Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO",
-            };
-            axios.post(transactionAPI, {
-                "custID": null,
-                "accountKey": "",
-                "payeeID": null,
-                "amount": null,
-                "eGift": false,
-                "message": "",
-            }).then(function (response) {
-                console.log(response)
-            }).catch(function (error) {
-                console.log(error);
-            });
-        }
-    }
-    )
     const initialFormData = Object.freeze({
-        "custID": null,
+        "custID": 0,
         "accountKey": "",
-        "payeeID": null,
-        "amount": null,
+        "payeeID": 0,
+        "amount": 0,
         "eGift": false,
         "message": "",
     })
@@ -38,12 +17,33 @@ function AddTransaction() {
 
     // const [formStatus, setFormStatus] = useState(null);
 
-    const handleSubmit = (e) => {
+    const handleChange = (e) => {
         setFormData({
           ...formData,
           [e.target.name]: e.target.value.trim(),
         });
+        console.log(formData)
     };
+
+    const handleSubmit = () => {
+        const data = {...formData};
+        console.log(data)
+        // useEffect(() => {
+        //     async function postTransaction() {
+        //         let transactionAPI = 'https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add'
+        //         axios.defaults.headers.common = {
+        //             "X-API-Key": "Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO",
+        //         };
+        //         axios.post(transactionAPI, {
+        //             data
+        //         }).then(function (response) {
+        //             console.log(response)
+        //         }).catch(function (error) {
+        //             console.log(error);
+        //         });
+        //     }
+        // }
+    }
 
     return (
         <>
@@ -51,28 +51,28 @@ function AddTransaction() {
             <form>
                 <div class="mb-3">
                     <label for="custID" class="form-label">Customer ID</label>
-                    <input type="text" id="custID"></input>
+                    <input type="text" id="custID" name="custID"></input>
                 </div>
                 <div class="mb-3">
                     <label for="accountKey" class="form-label">Account Key</label>
-                    <input type="text" id="accountKey"></input>
+                    <input type="text" id="accountKey" name="accountKey"></input>
                 </div>
                 <div class="mb-2">
                     <label for="payeeID" class="form-label">Payee ID</label>
-                    <input type="text" id="payeeID"></input>
+                    <input type="text" id="payeeID" name="payeeID"></input>
                 </div>
                 <div class="mb-2">
                     <label for="amount" class="form-label">Amount</label>
-                    <input type="text" id="amount"></input>
+                    <input type="text" id="amount" name="amount"></input>
                 </div>
                 <div class="mb-2">
                     <label for="eGift" class="form-label">eGift</label>
-                    <input type="text" id="eGift"></input>
+                    <input type="text" id="eGift" name="eGift"></input>
                     {/* true false */}
                 </div>
                 <div class="mb-2">
                     <label for="message" class="form-label">Message</label>
-                    <input type="text" id="message"></input>
+                    <input type="text" id="message" name="message"></input>
                 </div>
             </form>
             <button
