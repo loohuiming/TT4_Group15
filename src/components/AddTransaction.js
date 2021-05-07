957cad868de1040b6aaad4086f2e4fd032417cbc
loohuiming <43126456+loohuiming@users.noreply.github.com>
2021-05-07 13:20:54 +0800

    linked submit button to addTransaction api

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,89 +1,135 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 function AddTransaction() {
-    const initialFormData = Object.freeze({
-        "custID": 0,
-        "accountKey": "",
-        "payeeID": 0,
-        "amount": 0,
-        "eGift": false,
-        "message": "",
-    })
+  const initialFormData = Object.freeze({
+    custID: 0,
+    accountKey: "",
+    payeeID: 0,
+    amount: 0,
+    eGift: false,
+    message: "",
+  });
 
-    const [formData, setFormData] = useState(initialFormData);
+  const [formData, setFormData] = useState(initialFormData);
 
-    // const [formStatusType, setFormStatusType] = useState("");
+  // const [formStatusType, setFormStatusType] = useState("");
 
-    // const [formStatus, setFormStatus] = useState(null);
+  // const [formStatus, setFormStatus] = useState(null);
 
-    const handleChange = (e) => {
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value.trim(),
-        });
-        console.log(formData)
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    });
+    console.log(formData);
+  };
+
+  const handleSubmit = () => {
+    const data = { ...formData };
+    console.log(data);
+
+    const postTransaction = async () => {
+      try {
+        const transactionAPI =
+          "https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add";
+
+        fetch(transactionAPI, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "X-API-Key": "Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO"
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json); // output from API
+          });
+      } catch (e) {
+        console.error(e.message);
+      }
     };
 
-    const handleSubmit = () => {
-        const data = {...formData};
-        console.log(data)
-        // useEffect(() => {
-        //     async function postTransaction() {
-        //         let transactionAPI = 'https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add'
-        //         axios.defaults.headers.common = {
-        //             "X-API-Key": "Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO",
-        //         };
-        //         axios.post(transactionAPI, {
-        //             data
-        //         }).then(function (response) {
-        //             console.log(response)
-        //         }).catch(function (error) {
-        //             console.log(error);
-        //         });
-        //     }
-        // }
-    }
+    postTransaction();
+  };
 
-    return (
-        <>
-            <h1>Make a Transaction</h1>
-            <form>
-                <div class="mb-3">
-                    <label for="custID" class="form-label">Customer ID</label>
-                    <input type="text" id="custID" name="custID"></input>
-                </div>
-                <div class="mb-3">
-                    <label for="accountKey" class="form-label">Account Key</label>
-                    <input type="text" id="accountKey" name="accountKey"></input>
-                </div>
-                <div class="mb-2">
-                    <label for="payeeID" class="form-label">Payee ID</label>
-                    <input type="text" id="payeeID" name="payeeID"></input>
-                </div>
-                <div class="mb-2">
-                    <label for="amount" class="form-label">Amount</label>
-                    <input type="text" id="amount" name="amount"></input>
-                </div>
-                <div class="mb-2">
-                    <label for="eGift" class="form-label">eGift</label>
-                    <input type="text" id="eGift" name="eGift"></input>
-                    {/* true false */}
-                </div>
-                <div class="mb-2">
-                    <label for="message" class="form-label">Message</label>
-                    <input type="text" id="message" name="message"></input>
-                </div>
-            </form>
-            <button
-                type="submit"
-                className="button is-link"
-                onClick={() => handleSubmit()}
-            >
-                Make a transaction
-            </button>
-        </>
-    )
+  return (
+    <>
+      <h1>Make a Transaction</h1>
+      <div>
+        <div className="form-group">
+          <label htmlFor="custID">Customer ID</label>
+          <input
+            type="text"
+            className="form-control"
+            name="custID"
+            placeholder=""
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="accountKey">Account Key</label>
+          <input
+            type="text"
+            className="form-control"
+            name="accountKey"
+            placeholder=""
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="payeeID">Payee ID</label>
+          <input
+            type="text"
+            className="form-control"
+            name="payeeID"
+            placeholder=""
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="text"
+            className="form-control"
+            name="amount"
+            placeholder=""
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="eGift">eGift</label>
+          <input
+            type="text"
+            className="form-control"
+            name="eGift"
+            placeholder=""
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            type="text"
+            className="form-control"
+            name="message"
+            placeholder=""
+            onChange={handleChange}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-success"
+          onClick={() => handleSubmit()}
+        >
+          Make a transaction
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default AddTransaction;
