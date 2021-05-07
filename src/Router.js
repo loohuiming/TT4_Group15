474cb236c46018bb8c651cbb5e3211029c55337e
JTEO045 <JTEO045@e.ntu.edu.sg>
2021-05-07 15:23:04 +0800

    API Update

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-<<<<<<< Updated upstream
-import { Login } from './components/Login';
-import AddTransaction from './components/AddTransaction';
-=======
-import {Login} from './components.Login'
-import {Balance} from './components/Balance'
+import {Login} from './components/Login'
+import Balance from './components/Balance'
 // import TodoPage from "./components/todoPage";
->>>>>>> Stashed changes
 
 export default function Router() {
     const LoginContainer = () => (
@@ -19,11 +14,7 @@ export default function Router() {
       const DefaultContainer = () => (
         <div>
           <div className="container">
-<<<<<<< Updated upstream
-            <Route path="/addTransaction" component={AddTransaction} />
-=======
             {<Route path="/balance" component={Balance} />}
->>>>>>> Stashed changes
           </div>
         </div>
       );
@@ -35,11 +26,7 @@ export default function Router() {
           <Switch>
             <Route exact path="/" component={LoginContainer} />
             <Route component={DefaultContainer} />
-<<<<<<< Updated upstream
-            <Route path="/addTransaction" component={AddTransaction} />
-=======
             {<Route path="/balance" component={Balance} />}
->>>>>>> Stashed changes
           </Switch>
         </div>
       </BrowserRouter>
--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 
 class Balance extends Component {
+
+    postTransaction = async () => {
+        try {
+            axios.post(
+            "https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/accounts", JSON.stringify({
+                custID: 15,
+                accountKey: "owg0o82i-kaqt-7qfx-wrz6-2v0m8ey6tio"
+            }));
+
+            fetch(transactionAPI, {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    "X-API-Key": "Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO",
+                }
+            })
+                .then((response) => response.json())
+                    console.log(response)
+                };
+                
+        } catch (e) {
+            console.error(e.message);
+        }
+    };
+
     state = {
-        amount: 1000
+        availableBal: 10000
       };
+
+    checkBalance(){
+        return //for ID value, return availableBal amount in API.
+    }  
 
     afterTransaction = () => {
         this.setState({amount: this.state.amount - 1});
@@ -15,11 +45,13 @@ class Balance extends Component {
         return (
             <div>
                 <span>
-                    <p>Current Account Balance: {this.state.amount} </p>
+                    <h2>Current Account Available Balance: {this.state.availableBal} </h2>
                 </span>
             </div>
           );
     }
+
+
 }
  
 export default Balance;
