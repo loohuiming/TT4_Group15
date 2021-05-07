9e2e5846a83e227331baf13586e2ae495aea2b78
teoweisheng <83488530+teoweisheng@users.noreply.github.com>
2021-05-07 16:08:01 +0800

    Update viewTransaction.js
    commenting out

--- src/components/viewTransaction.js
+++ src/components/viewTransaction.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import axios from "axios";
+//import React from 'react';
+//import axios from "axios";
 //import {custID, accountKey} from "./components/Login.js";
 
-class ViewTransaction extends React.Component{
-    getData(){
-        axios.post(
-            'https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view',JSON.stringify({
-                custID: 15,
-                accountKey: "owg0o82i-kaqt-7qfx-wrz6-2v0m8ey6tio",
-            }),
-                {headers: {
-                    "x-api-key":"Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO",
-                    'Content-Type':'application/json',
-                    'Accept':"application/json"
-                }}
-            )
-        .then((response) => {
-            sessionStorage.setItem({
-                'data': response.data})
-            return 'data'
-        })
-        }
+//class ViewTransaction extends React.Component{
+//    getData(){
+//        axios.post(
+//           'https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view',JSON.stringify({
+//                custID: 15,
+//                accountKey: "owg0o82i-kaqt-7qfx-wrz6-2v0m8ey6tio",
+//            }),
+//                {headers: {
+//                    "x-api-key":"Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO",
+//                   'Content-Type':'application/json',
+//                    'Accept':"application/json"
+//                }}
+//            )
+//        .then((response) => {
+//            sessionStorage.setItem({
+//                'data': response.data})
+//            return 'data'
+//        })
+//        }
 
     // json2Table(json) {
     //     let cols = Object.keys(json[0]);
@@ -46,13 +46,13 @@ class ViewTransaction extends React.Component{
     //     return table;
     //     }
 
-    render(){
-        return(
-            <div>
-                {/* <p>{getData()}</p> */}
-            </div>
-        )
-    }
-}
+//    render(){
+//        return(
+//            <div>
+//                {/* <p>{getData()}</p> */}
+//            </div>
+//        )
+//    }
+//}
 
-export default ViewTransaction
+//export default ViewTransaction
