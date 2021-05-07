fd501ada7e17827b5e72847f7dd6911390b57534
Teo Wei Sheng <teoweisheng.work@gmail.com>
2021-05-07 11:19:58 +0800

    update vtrans

--- src/components/viewTransaction.js
+++ src/components/viewTransaction.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import axios from "axios"
+import React from 'react';
+import axios from "axios";
+import {custID, accountKey} from "/Login.js";
 
-a = axios.get(https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view)
-console.log(a)
-
-
-
+axios.post('https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view',{
+    custID: custID,
+    accountKey: accountKey,
+    xapikey: "Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO",
+})
+.then(response => {
+    console.log("Payee ID: ", response[1])
+})
 
