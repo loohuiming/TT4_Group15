5d03ff034d69ffe46f9d62bb2e713c838239646a
JTEO045 <JTEO045@e.ntu.edu.sg>
2021-05-07 15:45:43 +0800

    removed API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Router from './Router';
+import logo from '/dbs-logo.png';
+import'.App.css';
+
 
 function App() {
   return (
--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,56 +1,36 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import { TabContainer } from 'react-bootstrap';
 
 class Balance extends Component {
 
-    postTransaction = async () => {
-        try {
-            axios.post(
-            "https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/accounts", JSON.stringify({
-                custID: 15,
-                accountKey: "owg0o82i-kaqt-7qfx-wrz6-2v0m8ey6tio"
-            }));
-
-            fetch(transactionAPI, {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    "X-API-Key": "Qjstc0HrUl4agLzgkROfI9XqteNBxvdM5B8x0jWO",
-                }
-            })
-                .then((response) => response.json())
-                    console.log(response)
-                };
-                
-        } catch (e) {
-            console.error(e.message);
-        }
-    };
-
     state = {
+        accountName: 15,
+        accountNumber: 123456789,
         availableBal: 10000
       };
 
-    checkBalance(){
-        return //for ID value, return availableBal amount in API.
-    }  
+    // checkBalance(){
+    //     return //for ID value, return availableBal amount in API.
+    // }  
 
-    afterTransaction = () => {
-        this.setState({amount: this.state.amount - 1});
-    };
+    // afterTransaction = () => {
+    //     this.setState({amount: this.state.amount - 1});
+    // };
+    
 //deduct amount after transaction has been made
 
     render() { 
         return (
             <div>
                 <span>
+                    <h2>Account Name:{this.state.accountName}</h2>
+                    <h2>Account Number:{this.state.accountNumber}</h2>
                     <h2>Current Account Available Balance: {this.state.availableBal} </h2>
                 </span>
             </div>
           );
     }
-
 
 }
  
